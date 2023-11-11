b0dfa7f0b04765deb7bb55aa42433bc96285a129
Glitch (therapeutic-discovered-geese) <none>
2023-11-11 20:19:22 +0000

    🎾🏎 Checkpoint ./commands/qual-report.js:62824610/659 ./index.js:62824610/390

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import "dotenv/config";
 
 import { 
   Client,
+  Collection,
   Events,
   GatewayIntentBits
 } from "discord.js";
+
+import fs from 'node:fs';
+import path from 'node:path';
 
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
@@ -14,6 +18,13 @@ client.once(Events.ClientReady, c => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for const file of c
 
 
 
