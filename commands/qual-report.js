9c3203629ea40fb544f03560138ee053d581e70c
Glitch (therapeutic-discovered-geese) <none>
2023-11-11 20:30:35 +0000

    👖🌆 Checkpoint ./index.js:62824610/2887 ./commands/qual-report.js:62824610/2223

--- commands/qual-report.js
+++ commands/qual-report.js
@@ -1,8 +1,16 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder } from "discord.js";
 
-export default {
-  data: new SlashCommandBuilder().setName('qual-report').setDescription('Get qual report for present members'),
-  async execute(interaction) {
-    await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}\nThis server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
-  }
-}
+// export default {
+//   data: new SlashCommandBuilder().setName('qual-report').setDescription('Get qual report for present members'),
+//   execute:  async (interaction) => {
+//     await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}\nThis server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+//   }
+
+export const data = new SlashCommandBuilder()
+  .setName("qual-report")
+  .setDescription("Get qual report for present members");
+export const execute = async (interaction) => {
+  await interaction.reply(
+    `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}\nThis server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
+  );
+};
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 import "dotenv/config";
 
-import { 
-  Client,
-  Collection,
-  Events,
-  GatewayIntentBits
-} from "discord.js";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 
-import fs from 'node:fs';
-import path from 'node:path';
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, c => {
+client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
@@ -21,18 +19,24 @@ client.login(process.env.DISCORD_TOKEN);
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
-for const file of c
+(async function () {
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = await import(filePath);
 
-
-
-
-
-
-
-
-
-
-
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.error(
+        `[WARNING] The command at ${filePath} is missing required field ${
+          "data" in command ? "data" : "execute"
+        }`
+      );
+    }
+  }
+})();
