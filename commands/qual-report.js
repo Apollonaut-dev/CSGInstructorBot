340bc68528f618be483efda3121806dad33b5b7f
Glitch (therapeutic-discovered-geese) <none>
2023-11-11 23:49:04 +0000

    👍👄 Checkpoint ./commands/qual-report.js:62824610/225

--- commands/qual-report.js
+++ commands/qual-report.js
@@ -5,6 +5,11 @@ export const data = new SlashCommandBuilder()
   .setDescription("Get qual report for present members");
 export const execute = async (interaction) => {
   console.log('executing qual-report')
+  
+  const channel = interaction.guild.channels.cache.find(c => c.id == 1172378718853935138)
+  
+  console.log(channel);
+  
   await interaction.reply(
     `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}\nThis server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
   );
