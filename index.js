30dcb9166fc85620d7509213b738f9f51129e4f3
Glitch (therapeutic-discovered-geese) <none>
2023-11-11 23:00:58 +0000

    🚶🎬 Checkpoint ./index.js:62824610/4781

--- index.js
+++ index.js
@@ -4,29 +4,28 @@ import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 
 import fs from "node:fs";
 import path from "node:path";
-
 import { fileURLToPath } from "url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-console.log("Hello world");
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
-
-client.commands = new Collection();
-
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith(".js"));
 
 (async function () {
+  console.log("Hello world");
+
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+  client.once(Events.ClientReady, (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+  });
+
+  await client.login(process.env.DISCORD_TOKEN);
+
+  client.commands = new Collection();
+
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = await import(filePath);
@@ -41,12 +40,12 @@ const commandFiles = fs
       );
     }
   }
-  console.log('mapped commands to client. registering interaction listener');
+  console.log("mapped commands to client. registering interaction listener");
   client.on(Events.InteractionCreate, async (interaction) => {
     console.log("handling interaction");
     if (!interaction.isChatInputCommand()) return;
     const command = interaction.client.commands.get(interaction.commandName);
-    
+
     if (!command) {
       console.error(`No command matching ${interaction.commandName}`);
       return;
