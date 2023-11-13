54dd9d82ec0ce85991787c476dc24a359d131b8e
Glitch (therapeutic-discovered-geese) <none>
2023-11-13 19:58:31 +0000

    🚡📷 Checkpoint ./commands/qual-report.js:62824610/423

--- commands/qual-report.js
+++ commands/qual-report.js
@@ -14,7 +14,12 @@ import { modex_regex } from '../data/util.js';;
 
 export const data = new SlashCommandBuilder()
   .setName("qual-report")
-  .setDescription("Get qual report for present members");
+  .setDescription("Get qual report for present members")
+  .addStringOption(option => {
+    return option.setName('squadron')
+      .setDescription('Squadron the bot will report for')
+      .setRequired(true);
+  });
 
 export const execute = async (interaction) => {
   console.log('executing qual-report')
@@ -23,7 +28,13 @@ export const execute = async (interaction) => {
   
   const ready_room_vc = await interaction.guild.channels.fetch('676242520484741150');
   const members = await ready_room_vc.members;
+  
+  if (members.length == 0) {
+    interaction.reply(`224 ready room is empty`)
+  }
+  
   const nicknames = members.map(m => m.nickname ? m.nickname : m.user.username);
+
   const present_modices = nicknames.map(p => Number(p.match(modex_regex)));
   console.log(members.map((m) => m.nickname ? m.nickname : m.user.username));
   
