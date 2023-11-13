25a958e3264f4f838d7a50c09e8b05dc15b2464c
Glitch (therapeutic-discovered-geese) <none>
2023-11-13 20:21:53 +0000

    👁🌿 Checkpoint ./commands/qual-report.js:62824610/1857

--- commands/qual-report.js
+++ commands/qual-report.js
@@ -1,16 +1,28 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 import * as BengalReport from '../data/qual-report-224.js';
+// import * as JollyRogersReport from '../data/qual-report-103.js';
+// import * as TophattersReport from '../data/qual-report-14.js';
+// import * as SidewindersReport from '../data/qual-report-86.js';
+// import * as NightmaresReport form '../data/qual-report-513.js';
+
 import { modex_regex } from '../data/util.js';;
 
-// const SquadronChannelMap = {
-//   103: '676242923859214352',
-//   14: '676244125363994625',
-//   86: '676243960712265769',
-//   224: '676242520484741150',
-//   513: '938036587361615913',
-//   126: '1102420287405961267'
-// }
+const SquadronReadyRoomVCChannelMap = {
+  103: '676242923859214352',
+  14: '676244125363994625',
+  86: '676243960712265769',
+  224: '676242520484741150',
+  513: '938036587361615913'
+}
+
+const IPTextChannelSquadronMap = {
+  ['741017122796339251']: 103,
+  ['907489943218163742']: 14,
+  ['616728259098443787']: 86,
+  ['743379174588153958']: 224,
+  ['951884830491152424']: 513
+}
 
 export const data = new SlashCommandBuilder()
   .setName("qual-report")
@@ -23,13 +35,29 @@ export const data = new SlashCommandBuilder()
 export const execute = async (interaction) => {
   console.log('executing qual-report')
   
-  const command_channel = interaction.channel.id
+  
+  // const command_channel = interaction.channel.id
+  
+  const squadron_arg = interaction.options.getString('squadron');
+  
+  // select VC based on argument
+  if (squadron_arg && squadron_arg.length) {
+  
+  } 
+  // select VC based on which IP channel the command was executed from
+  else {
+    switch(interaction.channel.channelId) {
+        
+    }
+  }
   
   const ready_room_vc = await interaction.guild.channels.fetch('676242520484741150');
+  
+  
   const members = await ready_room_vc.members;
   
   if (members.length == 0) {
-    interaction.reply(`224 ready room is empty.`)
+    return interaction.reply(`224 ready room is empty.`);
   }
   
   const nicknames = members.map(m => m.nickname ? m.nickname : m.user.username);
